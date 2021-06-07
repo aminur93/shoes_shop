03d74716721fdcb9a1a5f0159ec53681e97e4b04
aminur <aminurrashid126@gmail.com>
2021-06-07 22:44:36 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::group(['middleware' => 'auth'], function (){
+
+    /*dashboard routes start*/
+    Route::get('/dashboard', 'HomeController@index')->name('dashboard');
+    /*dashboard routes end*/
+
+    /*category routes start*/
+    Route::get('category','Admin\CategoryController@index')->name('category');
+    Route::get('category/create','Admin\CategoryController@create')->name('category.create');
+    Route::post('category/store','Admin\CategoryController@store')->name('category.store');
+    Route::get('category/edit','Admin\CategoryController@edit')->name('category.edit');
+    Route::post('category/update','Admin\CategoryController@update')->name('category.update');
+    Route::delete('category/destroy/{id}','Admin\CategoryController@destroy')->name('category.destroy');
+    /*category routes end*/
+
+});
+
+
